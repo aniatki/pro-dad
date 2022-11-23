20cdb89ff88f67a445a97bcd9cdfbb862bec2562
Hasan Atki <djamblers@gmail.com>
2022-11-23 13:15:04 +0000

    Heroku Deployment

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 
 os.environ['DATABASE_URL'] = 'postgres://glblcktv:oOCY__OXfSwTIFNMBHvb6EgVTRtTZZA5@lucky.db.elephantsql.com/glblcktv'
 os.environ['SECRET_KEY'] = '12M06XaK#*Bc'
+os.environ['CLOUDINARY_URL']="cloudinary://485763762286817:zl_lsTaZ3wn6r3g3cVWim7pM5As@dvc5n2usx"
