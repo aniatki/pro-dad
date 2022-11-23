a7f4f9da10552362242cc61de710543e936002ab
Hasan Atki <djamblers@gmail.com>
2022-11-23 13:45:02 +0000

    Fix installed apps bug in settings.py

--- lashes/settings.py
+++ lashes/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['belle-lash.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['belle-lash.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage,'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'homepage',
