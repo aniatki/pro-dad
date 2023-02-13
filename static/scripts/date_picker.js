a9f3b53b6f00094be10ba107c1b8de473ef81c51
Hasan Atki <djamblers@gmail.com>
2023-02-13 17:17:16 +0000

    Refactor date_picker.js

--- static/scripts/date_picker.js
+++ static/scripts/date_picker.js
@@ -17,28 +17,34 @@ if (window.location.pathname === "/") {
         "16:00",
         "17:30",
     ]
-
+    
+    const appointmentsAvailable = [
+        "09:00",
+        "10:30",
+        "12:00",
+    ]
     populateTimeSlots(appointmentTimes)
 
     function populateTimeSlots(appTimesArray) {
         for (i in appTimesArray) {
             const option = document.createElement("option")
             option.value = appTimesArray[i]
+            const newList = appointmentsAvailable.filter(app => app != option.value) 
+            console.log(newList)
             option.innerText = appTimesArray[i]
             selectElement.append(option)
-            option.addEventListener('click', isTimeSlotAvailable(option))
         }
     }
 
     // Return selected timeslot
 
-    selectElement.addEventListener("change", (e) => e.target.value)
+    // selectElement.addEventListener("change", (e) => e.target.value)
 
     // Find out what date is picked
 
     function checkPickedDate() {}
 
-    // Find out if there is available appointments at selected date and get error messages, if any
+    // Find out if there is available appointments at selected date and get error messages, if any. Select next available date
     isDateAvailable(datePicker)
 
     function isDateAvailable(inputField) {
@@ -56,9 +62,10 @@ if (window.location.pathname === "/") {
             let choiceIndex = choice.getDay()
 
             if (choiceIndex === 6) {
-                if (date === 28 && month === 1) date = -1
-                if (date === 30) date = -1 // -1 + 2 = 1
-                if (date === 31) date = 0 // 0 + 2 = 2
+                if (date === 28 && month === 1 || 
+                    date === 29 && month === 1) date = -1
+                if (date === 30) date = -1
+                if (date === 31) date = 0
                 
                 inputField.value = `${year}-${month}-${date + 2}`
                 
@@ -66,9 +73,10 @@ if (window.location.pathname === "/") {
             }
             
             if (choiceIndex === 0) {
-                if (date === 28 && month === 1) date = 0
-                if (date === 30) date = 0 // 0 + 1 = 1
-                if (date === 31) date = 1 // 1 + 1 = 2
+                if (date === 28 && month === 1 || 
+                    date === 29 && month === 1) date = 0
+                if (date === 30) date = 0
+                if (date === 31) date = 1
             
                 inputField.value = `${year}-${month}-${date + 1}`
             
@@ -79,14 +87,15 @@ if (window.location.pathname === "/") {
         })
     }
 
-    // If not, select the next available date
-
-    function getNextDate() {}
-
     // Find out if there is available appointments at selected time
 
-    function isTimeSlotAvailable(opt) {
-        // if (opt != 'available') opt.setAttribute('disabled', 'true')
-        // console.log(opt)
+    function isTimeSlotAvailable(app) {
+        for (let i in app) {
+            if (appointmentTimes.includes(app[i])) {
+                const available = []
+                available.push(app[i])
+                return available
+            }
+            }
+        }
     }
-}
