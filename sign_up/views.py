08806dc5510f870ea9eb9d85f90d7e60c4fcb5cd
Hasan Atki <djamblers@gmail.com>
2023-02-22 01:38:39 +0000

    Add functionality to create users in /sign_up with custom forms

--- sign_up/views.py
+++ sign_up/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
 
 def sign_up(request):
-    return render(request, "sign_up/sign_up.html", {"title": "Sign Up"})
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegisterForm()
+        
+    return render(request, "sign_up/sign_up.html", {"title": "Sign Up", "form": form})
