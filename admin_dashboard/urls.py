1faf51297eb7c64125c37b0234acc129bac2c46e
Hasan Atki <djamblers@gmail.com>
2023-03-16 02:31:22 +0000

    Add Update functionality in admin panel for packages

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('admin_dashboard', views.admin_dashboard, name="admin_dashboard"),
     path('admin_packages', views.admin_packages, name="admin_packages"),
+    path('update_package/<package_id>', views.update_package, name="update_package"),
     path('delete_package/<package_id>', views.delete_package, name="delete_package"),
 
 ]
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Booking, Package
 from .forms import AddPackageForm
 from django.contrib import messages
-
+from django.http import HttpResponseRedirect
 
 
 @login_required
@@ -14,36 +14,49 @@ def admin_dashboard(request):
 
 @login_required
 def admin_packages(request):
-    """
-    Create and Read functionality for serving the custom admin page
-    """
-    packages = Package.objects.all() # View all packages - Read
-    
-    # Add a package - Create
+    packages = Package.objects.all()
     form = AddPackageForm()
+    
+    context = {
+        'title': 'Packages',
+        'packages': packages,
+        'form': form
+    }
+
     if request.method == 'POST':
         form = AddPackageForm(request.POST, request.FILES)
         if form.is_valid():
             package_name = request.POST.get('name')
             form.save()
             messages.add_message(request, messages.SUCCESS, f'Item <strong>{package_name}</strong> added successfully.')
+            return redirect('/add_package')
         else:
+            form = AddPackageForm
             if form.errors:
                 for field in form:
                     for error in field.errors:
                         messages.add_message(request, messages.WARNING, f'{field.value} {error}')
-            form = AddPackageForm()
 
-    return render(request, 'admin_dashboard/admin_packages.html', {"title": "Packages", 'packages':packages, 'form': form})
+    return render(request, 'admin_dashboard/admin_packages.html', context)
+
 
 def delete_package(request, package_id):
     package = Package.objects.get(pk=package_id)
     package.delete()
-    messages.add_message(request, messages.SUCCESS, f'{package} was successfully deleted.')
+    messages.add_message(request, messages.SUCCESS, f'{package} was deleted.')
     return redirect('admin_packages')
+
 
 def update_package(request, package_id):
     package = Package.objects.get(pk=package_id)
-    
-    messages.add_message(request, messages.SUCCESS, f'{package} was saved.')
-    return redirect('admin_packages')
+    form = AddPackageForm(request.POST or None, instance=package)
+    if form.is_valid():
+        form.save()
+        messages.add_message(request, messages.SUCCESS, f'{package} was updated.')
+        return redirect('admin_packages')
+    context = {
+        'title': f'Edit {package.name}',
+        'package': package,
+        'form': form
+    }
+    return render(request, 'admin_dashboard/update_package.html', context)
