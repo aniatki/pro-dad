5d106564ca105c7f348570c7b50e67f7f66121f0
Hasan Atki <djamblers@gmail.com>
2023-03-22 17:11:23 +0000

    Removed Whitenoise from the list of installed apps and from requirements.txt file, as advised from Tutor Assistance

--- lashes/settings.py
+++ lashes/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['belle-lash.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
