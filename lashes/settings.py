2e4c0a425df8a0bf04fde2e9dd8b4efabddbe62d
Hasan Atki <djamblers@gmail.com>
2023-03-22 17:23:56 +0000

    Removed cloudinary config object from settings.py as advised from Tutor Assistance

--- lashes/settings.py
+++ lashes/settings.py
@@ -158,12 +158,6 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),  # required
-    'API_KEY': os.environ.get('API_KEY'),  # required
-    'API_SECRET': os.environ.get('API_SECRET'),  # required
-}
-
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
